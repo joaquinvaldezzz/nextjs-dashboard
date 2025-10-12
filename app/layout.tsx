d2778e92e46cf4353f2b5e49f17137f5cfbf75ab
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-12 12:01:58 +0800

    feat: use fonts from Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { ReactNode } from "react";
+import clsx from "clsx";
+
+import { inter } from "./ui/fonts";
+
+import "./ui/global.css";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, "antialiased")}>{children}</body>
     </html>
   );
 }
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,9 +1,11 @@
-import { lusitana } from "@/app/ui/fonts";
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+
+import { lusitana } from "./fonts";
 
 export default function AcmeLogo() {
   return (
-    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+    <div className={clsx(lusitana.className, "flex flex-row items-center leading-none text-white")}>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
     </div>
