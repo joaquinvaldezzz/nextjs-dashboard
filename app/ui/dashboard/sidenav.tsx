2d9637f61027269c9037e43574bd41403665e880
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-11-06 22:25:50 +0800

    feat: add sign out functionality

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { signOut } from "@/auth";
 import { PowerIcon } from "@heroicons/react/24/outline";
 
 import AcmeLogo from "@/app/ui/acme-logo";
@@ -18,7 +19,13 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-y-2 md:space-x-0">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block" />
-        <form>
+        <form
+          action={async () => {
+            "use server";
+
+            await signOut({ redirectTo: "/" });
+          }}
+        >
           <button
             className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
             type="button"
