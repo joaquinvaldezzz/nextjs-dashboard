a1f6327ba9f7316d32f00247d0c9297e8fc179bb
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-11-06 22:25:50 +0800

    feat: add NextAuth configuration

---
+++ proxy.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+
+import { authConfig } from "./auth.config";
+
+export const config = {
+  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
+
+export default NextAuth(authConfig).auth;
